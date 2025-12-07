422b313a9cedea83509e66dfae113c979f8f2e31
surajrajbhar <suraj.k.rajbhar28@gmail.com>
2025-12-08 00:09:04 +0530

    initial commit

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -17,8 +17,8 @@ const attachCookiesToResponse = ({ res, user }) => {
   res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    // secure: process.env.NODE_ENV === "production",
-    secure: true,
+    secure: process.env.NODE_ENV === "production",
+    // secure: true,
     signed: true,
     sameSite: "None",
   });
