efd50b5fdaa73d8f5c09ea7e9508cf6364572b33
surajrajbhar <suraj.k.rajbhar28@gmail.com>
2025-12-08 07:44:34 +0530

    initial commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -41,21 +41,21 @@ const Home = () => {
 
   const handlePageChange = (event, value) => {
     setPage(value);
-    setParams({ search, page: value, sort, order });
+    setParams({ input, page: value, sort, order });
   };
 
   const handleSortChange = (event) => {
     const value = event.target.value;
     setSort(value);
     setPage(1);
-    setParams({ search, page: 1, sort: value, order });
+    setParams({ input, page: 1, sort: value, order });
   };
 
   const handleOrderChange = (event) => {
     const value = event.target.value;
     setOrder(value);
     setPage(1);
-    setParams({ search, page: 1, sort, order: value });
+    setParams({ input, page: 1, sort, order: value });
   };
 
   if (isError)
